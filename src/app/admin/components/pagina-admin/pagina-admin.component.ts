a0ff1cc7b853ac206b9b9f5e70999255db557b8a
Natália Cavaillier <nataliaabreu2000@hotmail.com>
2022-07-11 14:13:43 -0300

    Adição do menu específico para Admin

---
+++ src/app/admin/components/pagina-admin/pagina-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-admin',
+  templateUrl: './pagina-admin.component.html',
+  styleUrls: ['./pagina-admin.component.css']
+})
+export class PaginaAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
